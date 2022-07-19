c31ca954f077ed4f1e8a8a65786e6538bd09d7a4
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-07-19 02:02:31 +0100

    update images, index.js and global.css

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Layout from '../components/layout'
 
 export default function Home() {
   return (
     <Layout>
       <Head>
-        <title>Home</title>
+        <title>Home | UI Pal</title>
+        <meta name="description" content="UI Pal is a collection of UI components for React. Download the source codes of your favorite component with the click of a button 😁" />
       </Head>
     </Layout>
   )
