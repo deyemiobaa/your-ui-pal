0aa355491b14b69230a0b4552bc7ea5acc390ac0
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-07-20 00:19:47 +0100

    switch theme changer icon

--- helpers/theme-change-icon.jsx
+++ helpers/theme-change-icon.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import {MdOutlineLightMode} from 'react-icons/md';
-import {BsMoonStarsFill} from 'react-icons/bs';
+import {BsMoonStars} from 'react-icons/bs';
 
 export default function ThemeChangeIcon() {
   const [theme, setTheme] = useState('light');
@@ -11,7 +11,7 @@ export default function ThemeChangeIcon() {
 
   return (
     <div className="theme-change" onClick={toggleTheme}>
-      {theme === 'light' ? <MdOutlineLightMode className='text-3xl' /> : <BsMoonStarsFill className='text-3xl' />}
+      {theme === 'light' ? <MdOutlineLightMode className='text-2xl' /> : <BsMoonStars className='text-2xl' />}
     </div>
   );
 }
