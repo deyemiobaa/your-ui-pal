c0fc20c096a9eb792ba9bbf7255f6255073781bc
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-07-21 15:20:45 +0100

    set up theme changer with next-themes

--- helpers/theme-change-icon.jsx
+++ helpers/theme-change-icon.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react';
-import {MdOutlineLightMode} from 'react-icons/md';
-import {BsMoonStars} from 'react-icons/bs';
+import { useState, useEffect } from 'react'
+import { useTheme } from 'next-themes'
+import { MdOutlineLightMode } from 'react-icons/md';
+import { BsMoonStars } from 'react-icons/bs';
 
 export default function ThemeChangeIcon() {
-  const [theme, setTheme] = useState('light');
+  const [mounted, setMounted] = useState(false)
+  const { theme, setTheme } = useTheme()
 
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+  
   const toggleTheme = () => {
     setTheme(theme === 'light' ? 'dark' : 'light');
   }
 
+  if (!mounted) {
+    return <MdOutlineLightMode className='text-2xl' />
+  }
+  
   return (
     <div className="theme-change" onClick={toggleTheme}>
       {theme === 'light' ? <MdOutlineLightMode className='text-2xl' /> : <BsMoonStars className='text-2xl' />}
